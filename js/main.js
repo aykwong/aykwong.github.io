ff2a0c5496b3768b764ce33d991f5e7abefe2c7f
Kwong <aykwong93@gmail.com>
2018-09-10 23:05:39 -0400

    Changed logo, cleaned up code

--- js/main.js
+++ js/main.js
@@ -1,57 +1,33 @@
 
-$(document).ready(function(){
-	"use strict";
+$(document).ready(function () {
+    "use strict";
 
-	var window_width 	 = $(window).width(),
-	window_height 		 = window.innerHeight,
-	header_height 		 = $(".default-header").height(),
-	header_height_static = $(".site-header.static").outerHeight(),
-	fitscreen 			 = window_height - header_height;
+    var window_width = $(window).width(),
+        window_height = window.innerHeight,
+        header_height = $(".default-header").height(),
+        header_height_static = $(".site-header.static").outerHeight(),
+        fitscreen = window_height - header_height;
 
 
-	$(".fullscreen").css("height", window_height)
-	$(".fitscreen").css("height", fitscreen);
+    $(".fullscreen").css("height", window_height)
+    $(".fitscreen").css("height", fitscreen);
 
-     
-     // -------   Active Mobile Menu-----//
 
-    $(".menu-bar").on('click', function(e){
+    // -------   Active Mobile Menu-----//
+
+    $(".menu-bar").on('click', function (e) {
         e.preventDefault();
         $("nav").toggleClass('hide');
         $("span", this).toggleClass("lnr-menu lnr-cross");
         $(".main-menu").addClass('mobile-menu');
     });
-     
-    $('select').niceSelect();
-    $('.img-pop-up').magnificPopup({
-        type: 'image',
-        gallery:{
-        enabled:true
-        }
+
+    $("body").find("a").click(function (e) {
+        e.preventDefault();
+
+        var section = $(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(section).offset().top
+        }, 1000);
     });
-
-    $('.active-bottle-carousel').owlCarousel({
-        items:1,
-        loop:true,
-        nav: false,
-        autoplay: true,
-        autoplayTimeout:3000,
-        autoplayHoverPause:true
-    });
-
-    $('.play-btn').magnificPopup({
-        disableOn: 700,
-        type: 'iframe',
-        mainClass: 'mfp-fade',
-        removalDelay: 160,
-        preloader: false,
-        fixedContentPos: false
-    });
-
-
-    $(document).ready(function() {
-        $('#mc_embed_signup').find('form').ajaxChimp();
-    });      
-    // -------   Mail Send ajax
-
- });
+});
